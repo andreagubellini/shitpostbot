6db4e72d588f54c7abd60300b5cc09799351707f
Filippo Castelli <castelli@lens.unifi.it>
2020-04-14 15:18:23 +0200

    added configuration file, updated README

--- bot.py
+++ bot.py
@@ -1,19 +1,55 @@
-import discord
+import sys
+import yaml
+from argparse import ArgumentParser
+from pathlib import Path
+#import pdb
+
 from discord.ext import commands
 from scraper import scraper
 
-client = commands.Bot(command_prefix='.')
-token = 'my-token'
 
-@client.event
-async def on_ready():
-    print('bot is ready.')
-
-@client.event
-async def on_message(message):
-    if message.content.startswith('!shitpost'):
-        channel = client.get_channel(int('my-channel-id'))
-        img = scraper.scrape()
-        await channel.send("Ecco la tua daily dose di shitpost: \n"+img)
+def main():
+    parser = ArgumentParser()
+    # token as path to file
+    parser.add_argument(
+        "-f",
+        "--config",
+        action="store",
+        type=str,
+        dest="cfg_fpath",
+        help="Path to config file"
+        )
+    
+    args = parser.parse_args()
+    
+    # print help and exit if no arguments are passed
+    if len(sys.argv) == 1:
+        parser.print_help(sys.stderr)
+        sys.exit(1)
         
-client.run(token)
+    cfg_fpath = Path(args.cfg_fpath)
+    
+    with cfg_fpath.open(mode="r") as ymlfile:
+        cfg = yaml.load(ymlfile, Loader=yaml.SafeLoader)
+        
+    token = cfg["bot"]["token"]
+    ch_id = cfg["bot"]["channelid"]
+    boards = cfg["chan"]["boards"]
+    
+    client = commands.Bot(command_prefix='.')
+    @client.event
+    async def on_ready():
+        print('bot is ready.')
+        
+    @client.event
+    async def on_message(message):
+        if message.content.startswith('!shitpost'):
+            #pdb.set_trace()
+            channel = client.get_channel(int(ch_id))
+            img = scraper.scrape(boards)
+            await channel.send("Ecco la tua daily dose di shitpost: \n"+img)
+    
+    client.run(token)
+
+if __name__ == "__main__":
+    main()
